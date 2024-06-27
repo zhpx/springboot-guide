7db23fe464fd83ca56603b3c60d0ab6eadd2702b
zhpx <zhpx@sina.cn>
2024-06-27 18:01:36 +0800

    init

---
+++ src/main/java/com/zhpx/springbootguide/SpringbootGuideApplication.java
@@ -0,0 +1,13 @@
+package com.zhpx.springbootguide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGuideApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zhpx/springbootguide/SpringbootGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zhpx.springbootguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
